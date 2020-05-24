51120dc89fa41f90ae7984becd1dafd8aefe204c
Andreas Ellwanger <andreas.ellwanger@campus.lmu.de>
2020-05-24 15:36:35 +0200

    Add roboto font, add hero section

--- components/header.js
+++ components/header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Link from 'next/link';
+// import Link from 'next/link';
 import { LinkWithSound } from 'components/link-with-sound';
 import EmailIcon from 'assets/svg/email.svg';
 import GitHubIcon from 'assets/svg/github.svg';
@@ -9,9 +9,10 @@ export const Header = () => {
 	return (
 		<header className="">
 			<div className="flex flex-row container items-center justify-between mx-auto my-12 px-4 sm:px-0">
-				<Link href="/">
-					<a className="font-bold text-2xl tracking-tighter">Andreas Ellwanger</a>
-				</Link>
+				{/* <Link href="/">
+					<a className="font-bold text-2xl tracking-tighter">.</a>
+				</Link> */}
+				<div></div>
 				<div className="flex flex-row">
 					<LinkWithSound css="ml-2 text-gray-800 hover:text-black w-8 h-8 ml-5" href="mailto:contact@onandoff.io">
 						<EmailIcon />
