2b715a657b818ff314bbd0aaaf30831da9169faf
Andreas Ellwanger <andreas.ellwanger@campus.lmu.de>
2021-09-01 17:57:32 +0200

    Upgrade Next, Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,57 +1,10 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
-const colors = require('tailwindcss/colors')
 
 module.exports = {
   mode: 'jit',
-  purge: {
-    content: ['./components/**/*.{js,jsx,ts,tsx,mdx}', './pages/**/*.{js,jsx,ts,tsx,mdx}', './public/**/*.html'],
-    options: {
-      whitelist: ['fill-current', 'stroke-current', 'inline-block', 'w-full', 'h-full'],
-    },
-  },
-  darkMode: 'class',
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}', './public/**/*.html'],
+  darkMode: 'class', // or 'media' or 'false'
   theme: {
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-
-      black: '#000',
-      white: '#fff',
-
-      amber: colors.amber,
-      blue: colors.blue,
-      cyan: colors.cyan,
-      emerald: colors.emerald,
-      fuchsia: colors.fuchsia,
-      gray: colors.coolGray,
-      green: colors.green,
-      indigo: colors.indigo,
-      'light-blue': colors.lightBlue,
-      lime: colors.lime,
-      orange: {
-        ...colors.orange,
-        1000: '#4a2008',
-      },
-      pink: {
-        ...colors.pink,
-        1000: '#460d25',
-      },
-      purple: colors.purple,
-      red: colors.red,
-      rose: colors.rose,
-      teal: colors.teal,
-      violet: colors.violet,
-      yellow: colors.yellow,
-
-      code: {
-        punctuation: '#A1E8FF',
-        tag: '#D58FFF',
-        'attr-name': '#4BD0FB',
-        'attr-value': '#A2F679',
-        string: '#A2F679',
-        highlight: 'rgba(134, 239, 172, 0.25)',
-      },
-    },
     extend: {
       colors: {
         'my-black': '#010101',
@@ -61,6 +14,18 @@ module.exports = {
         'my-blue': '#0029ff',
         celonis: '#5cfe50',
         lmu: '#009440',
+        transparent: 'transparent',
+        current: 'currentColor',
+        black: '#000',
+        white: '#fff',
+        code: {
+          punctuation: '#A1E8FF',
+          tag: '#D58FFF',
+          'attr-name': '#4BD0FB',
+          'attr-value': '#A2F679',
+          string: '#A2F679',
+          highlight: 'rgba(134, 239, 172, 0.25)',
+        },
       },
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
@@ -69,6 +34,9 @@ module.exports = {
         system: defaultTheme.fontFamily.sans,
       },
     },
+  },
+  variants: {
+    extend: {},
   },
   plugins: [
     require('@tailwindcss/typography'), // https://tailwindcss.com/docs/typography-plugin
