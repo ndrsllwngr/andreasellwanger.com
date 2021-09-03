4c09213d35bb7195b828b4c896dddbbf3dea599c
Andreas Ellwanger <andreas.ellwanger@campus.lmu.de>
2021-09-03 21:49:25 +0200

    Add default text-color

--- components/layout.js
+++ components/layout.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 export const Layout = ({ children }) => {
-  return <div className="">{children}</div>
+  return <div className="text-black">{children}</div>
 }
 
 Layout.propTypes = {
