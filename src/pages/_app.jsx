0cb9c4d96c22dc0bf31c0e26b854fdb7e914806a
Andreas Ellwanger <a.ellwanger@celonis.com>
2023-01-08 12:58:41 +0100

    update deps

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -6,6 +6,7 @@ import { Header } from '@/components/Header'
 import '@/styles/tailwind.css'
 import 'focus-visible'
 import { ThemeProvider } from 'next-themes'
+import { Provider as BalancerProvider } from 'react-wrap-balancer'
 
 function usePrevious(value) {
   let ref = useRef()
@@ -27,18 +28,20 @@ export default function App({ Component, pageProps, router }) {
         enableSystem={false}
         defaultTheme={'dark'}
       >
-        <div className="fixed inset-0 flex justify-center sm:px-8">
-          <div className="flex w-full max-w-7xl lg:px-8">
-            <div className="w-full bg-white ring-1 ring-zinc-100 dark:bg-zinc-900 dark:ring-zinc-300/20" />
+        <BalancerProvider>
+          <div className="fixed inset-0 flex justify-center sm:px-8">
+            <div className="flex w-full max-w-7xl lg:px-8">
+              <div className="w-full bg-white ring-1 ring-zinc-100 dark:bg-zinc-900 dark:ring-zinc-300/20" />
+            </div>
           </div>
-        </div>
-        <div className="relative">
-          <Header />
-          <main>
-            <Component previousPathname={previousPathname} {...pageProps} />
-          </main>
-          <Footer />
-        </div>
+          <div className="relative">
+            <Header />
+            <main>
+              <Component previousPathname={previousPathname} {...pageProps} />
+            </main>
+            <Footer />
+          </div>
+        </BalancerProvider>
       </ThemeProvider>
     </>
   )
