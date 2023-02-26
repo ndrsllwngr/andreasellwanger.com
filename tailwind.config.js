6c58e4ffdd10457ae60d8a10be7cd28bc15fa480
Andreas Ellwanger <a.ellwanger@celonis.com>
2023-02-26 16:03:37 +0100

    configure tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx}'],
-  darkMode: 'class',
-  plugins: [require('@tailwindcss/typography')],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 }
