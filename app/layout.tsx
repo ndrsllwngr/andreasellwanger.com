beb56a05b8ce84f89ff6d5e56c672c50663febc6
Andreas Ellwanger <a.ellwanger@celonis.com>
2023-03-04 20:57:11 +0100

    update favicon, gitignore

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="h-full antialiased js-focus-visible dark">
+      <link rel="icon" href="/favicon.ico" sizes="any" />
+      <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+      <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+      <link rel="manifest" href="/site.webmanifest" />
+      <meta name="theme-color" content="#171717"></meta>
       <body
         className={`${inter.variable} ${caveat.variable} font-sans flex h-full flex-col overflow-y-scroll bg-neutral-900 text-gray-200`}
       >
